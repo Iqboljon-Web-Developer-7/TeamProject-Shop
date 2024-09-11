bbd2dcf0489ddea96db1450a054e5883ccc5e142
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-09-11 06:09:38 -0700

    react

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="bg-green-200 py-4 px-6">header</header>;
+  return <header className="bg-green-200 py-4 px-6">header
+  <h1>ee</h1>
+  </header>;
 };
 
 export default Header;
