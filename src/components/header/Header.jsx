a059ee866f1dddc75e5e710f847e4e57f156db21
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-09-11 10:38:55 -0700

    react

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+  const toggleDarkMode = () => {
+    window.document.body.classList.toggle("dark");
+    console.log(1);
+    
+  };
 const Header = () => {
-  return <header className="bg-green-200 py-4 px-6">head432e3r</header>;
+  return(
+   <header>
+      <h1 className="text-green-300 text-center text-6xl p-3 my-8 dark:text-white dark:bg-black">
+        Hello
+      </h1>
+      <button
+        onClick={toggleDarkMode}
+        className="p-4 bg-green-400 text-white rounded-lg"
+      >
+        Dark Toggle
+      </button>
+    </header>
+  );
 };
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/main.scss";
 import { BrowserRouter } from "react-router-dom";
-
 import Pages from "./pages/pages";
-
 import { Provider } from "react-redux";
 import { store } from "./redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className=" dark:text-white dark:bg-black min-h-screen w-screen">
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -16,4 +15,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
+  </div>
 );
