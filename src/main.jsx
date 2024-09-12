1b193a7642161bd8b343e19fc63c028bf6165ba5
Iqboljon <iqboljon@Iqboljons-MacBook-Pro.local>
2024-09-12 13:23:37 +0500

    suspense added

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/main.scss";
 import { BrowserRouter } from "react-router-dom";
 
-import Pages from "./pages/pages";
+const Pages = lazy(() => import("@/pages/pages"));
 
 import { Provider } from "react-redux";
 import { store } from "./redux";
+import InitialLoading from "./components/initialLoading/InitialLoading";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Pages />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <Suspense fallback={<InitialLoading />}>
+        <BrowserRouter>
+          <Pages />
+        </BrowserRouter>
+      </Suspense>
+    </Provider>
   </React.StrictMode>
 );
