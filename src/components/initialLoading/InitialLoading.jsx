c0b5f2ccdb8beda7c1696d76650f4d3c1a6f5f47
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-09-12 14:56:28 +0500

    updated

--- src/components/initialLoading/InitialLoading.jsx
+++ src/components/initialLoading/InitialLoading.jsx
@@ -1,11 +1,8 @@
-import { CircularProgress } from "@mui/material";
 import React from "react";
 
 const InitialLoading = () => {
   return (
     <div className="h-screen w-screen grid place-items-center">
-      {/* <CircularProgress color="success" /> */}
-      {/* /* From Uiverse.io by JkHuger /  */}
       <div class="loader">
         <div class="square" id="sq1"></div>
         <div class="square" id="sq2"></div>
