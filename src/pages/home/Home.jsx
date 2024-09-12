577930485f7f836c29b894d3f954e62619a3db39
Iqboljon <iqboljon@Iqboljons-MacBook-Pro.local>
2024-09-12 12:47:23 +0500

    updated code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
 import Brands from "@/components/brands/Brands";
-import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <Header />
       <Hero />
       <Brands />
     </>
