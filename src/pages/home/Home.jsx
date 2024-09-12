5b4497ed4720e97367a43f35472b9fefacfdba71
Iqboljon <iqboljon@Iqboljons-MacBook-Pro.local>
2024-09-12 12:37:17 +0500

    deleted unneccary files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import AddCard from "@/components/addCard/AddCard";
 import Brands from "@/components/brands/Brands";
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
@@ -8,7 +7,6 @@ const Home = () => {
   return (
     <section>
       <Header />
-      {/* <AddCard /> */}
       <Hero />
       <Brands />
     </section>
