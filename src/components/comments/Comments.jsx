d0778967f78b233e7ff09b20f1ce53ce504d1d82
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-09-12 15:36:31 +0500

    swiper

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          "@0.9": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          "@1.2": {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Autoplay, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- src/components/dressStyles/DressStyles.jsx
+++ src/components/dressStyles/DressStyles.jsx
@@ -8,14 +8,14 @@ import img4 from "@/assets/dressStyle/grid-4.png";
 const DressStyles = () => {
   return (
     <section className="mx-4 mt-20 py-[4.375rem] bg-[#F0F0F0] rounded-3xl">
-      <div className="wrapper max-w-7xl">
+      <div className="wrapper max-w-[45rem] lg:max-w-[90rem]">
         <h2 className="text-4xl text-center font-bold">
           BROWSE BY DRESS STYLE
         </h2>
-        <div className="grid grid-cols-5 gap-4 mt-16">
+        <div className="grid lg:grid-cols-5 gap-4 mt-16">
           <div
             style={{ backgroundImage: `url(${img1})` }}
-            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-2"
+            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-3 lg:col-span-2"
           >
             <h3 className="text-4xl font-bold">Casual</h3>
           </div>
@@ -33,7 +33,7 @@ const DressStyles = () => {
           </div>
           <div
             style={{ backgroundImage: `url(${img4})` }}
-            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-2"
+            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem]  col-span-3 lg:col-span-2"
           >
             <h3 className="text-4xl font-bold">Gym</h3>
           </div>
