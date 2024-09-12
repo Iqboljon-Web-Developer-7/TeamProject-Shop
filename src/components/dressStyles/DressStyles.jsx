1f1b6ec0b212973a55587161cc05f09c0588a7e7
Iqboljon <iqboljon@Iqboljons-MacBook-Pro.local>
2024-09-12 14:39:38 +0500

    updated

--- src/components/dressStyles/DressStyles.jsx
+++ src/components/dressStyles/DressStyles.jsx
@@ -7,11 +7,36 @@ import img4 from "@/assets/dressStyle/grid-4.png";
 
 const DressStyles = () => {
   return (
-    <section className="wrapper m-4 py-[4.375rem] bg-[#F0F0F0] rounded-3xl">
-      <h2 className="text-4xl text-center font-bold">BROWSE BY DRESS STYLE</h2>
-      <div className="grid grid-cols-[3fr_6fr]">
-        <div>
-          <h3>Casual</h3>
+    <section className="mx-4 mt-20 py-[4.375rem] bg-[#F0F0F0] rounded-3xl">
+      <div className="wrapper max-w-7xl">
+        <h2 className="text-4xl text-center font-bold">
+          BROWSE BY DRESS STYLE
+        </h2>
+        <div className="grid grid-cols-5 gap-4 mt-16">
+          <div
+            style={{ backgroundImage: `url(${img1})` }}
+            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-2"
+          >
+            <h3 className="text-4xl font-bold">Casual</h3>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${img2})` }}
+            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-3"
+          >
+            <h3 className="text-4xl font-bold">Formal</h3>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${img3})` }}
+            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-3"
+          >
+            <h3 className="text-4xl font-bold">Party</h3>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${img4})` }}
+            className="bg-no-repeat bg-right-top py-7 px-9 rounded-xl bg-white min-h-[18rem] col-span-2"
+          >
+            <h3 className="text-4xl font-bold">Gym</h3>
+          </div>
         </div>
       </div>
     </section>
